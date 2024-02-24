48f44b14928dbde6d4dc55017fc5f8d73a1dcf23
Gordon Erlebacher <gordon.erlebach@gmail.com>
2024-02-24 14:47:39 -0500

    Student templates
    No answers included.

---
+++ part1.py
@@ -0,0 +1,79 @@
+import myplots as myplt
+import time
+import warnings
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import cluster, datasets, mixture
+from sklearn.datasets import make_blobs
+from sklearn.neighbors import kneighbors_graph
+from sklearn.preprocessing import StandardScaler
+from itertools import cycle, islice
+import scipy.io as io
+from scipy.cluster.hierarchy import dendrogram, linkage  #
+
+# import plotly.figure_factory as ff
+import math
+from sklearn.cluster import AgglomerativeClustering
+import pickle
+import utils as u
+
+
+# ----------------------------------------------------------------------
+"""
+Part 1: 
+Evaluation of k-Means over Diverse Datasets: 
+In the first task, you will explore how k-Means perform on datasets with diverse structure.
+"""
+
+
+def compute():
+    answers = {}
+
+    """
+    A.	Load the following 5 datasets with 100 samples each: noisy_circles (nc), noisy_moons (nm), blobs with varied variances (bvv), Anisotropicly distributed data (add), blobs (b). Use the parameters from (https://scikit-learn.org/stable/auto_examples/cluster/plot_cluster_comparison.html), with any random state. (with random_state = 42). Not setting the correct random_state will prevent me from checking your results.
+    """
+
+    # Dictionary of 5 datasets. e.g., dct["nc"] = [data, labels]
+    # keys: 'nc', 'nm', 'bvv', 'add', 'b' (abbreviated datasets)
+    dct = answers["1A: datasets"] = {}
+
+    """
+   B. Write a function called fit_kmeans that takes dataset (before any processing on it), i.e., pair of (data, label) Numpy arrays, and the number of clusters as arguments, and returns the predicted labels from k-means clustering. Use the init='random' argument and make sure to standardize the data (see StandardScaler transform), prior to fitting the KMeans estimator.
+    """
+
+    # dct value:  the `fit_kmeans` function
+    dct = answers["1B: fit_kmeans"] = lambda: None
+
+    """
+    C.	Make a big figure (4 rows x 5 columns) of scatter plots (where points are colored by predicted label) with each column corresponding to the datasets generated in part 1.A, and each row being k=[2,3,5,10] different number of clusters. Which datasets does k-means seem to produce correct clusters for (assuming the right number of k is specified) and for which datasets does k-means fail for all values of k? 
+    
+    Create a pdf of the plots and return in your report. 
+    """
+
+    # dct value: return a list of the abbreviated dataset names
+    # abbreviations: 'nc', 'nm', 'bvv', 'add', 'b'
+    dct = answers["1C: cluster successes"] = [""]
+
+    # dct value: list of dataset abbreviations
+    dct = answers["1C: cluster failures"] = [""]
+
+    """
+    D. Repeat 1.C a few times and comment on which (if any) datasets seem to be sensitive to the choice of initialization for the k=2,3 cases. You do not need to add the additional plots to your report.
+
+    Create a pdf of the plots and return in your report. 
+    """
+
+    # dct value: list of dataset abbreviations
+    # Look at your plots, and return your answers.
+    # The plot is part of your report, a pdf file name "report.pdf", in your repository.
+    dct = answers["1D: datasets sensitive to initialization"] = [""]
+
+    return answers
+
+
+# ----------------------------------------------------------------------
+if __name__ == "__main__":
+    answers = compute()
+
+    with open("part1.pkl", "wb") as f:
+        pickle.dump(answers, f)
